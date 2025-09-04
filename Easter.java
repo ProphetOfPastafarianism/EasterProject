0cf78056d37438422f16bdd59266e9b1f1fc57e4
ProphetOfPastafarianism <magnushuston123@gmail.com>
2025-09-04 18:30:53 +0000

    done

--- Easter.java
+++ Easter.java
@@ -1,6 +1,18 @@
 public class Easter{
     public static void main(String[] args) {
         //gonna give you the day easter is based on the year you give.
-   
+   int y=2003;
+   int a=y%19;
+   System.out.println("a= "a);
+   int b=y/20;
+   System.out.println("b= ");
+   int c=y%20;
+   System.out.println("c= ");
+
+
+
     }
-}
+
+    
+    }
+
