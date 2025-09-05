563d3a7645b6195b398c7afdc74aa27272609d05
ProphetOfPastafarianism <magnushuston123@gmail.com>
2025-09-05 17:30:36 +0000

    gfgfd

--- Easter.java
+++ Easter.java
@@ -1,18 +1,42 @@
+import java.util.*;
 public class Easter{
-    public static void main(String[] args) {
-        //gonna give you the day easter is based on the year you give.
-   int y=2003;
-   int a=y%19;
-   System.out.println("a= "a);
-   int b=y/20;
-   System.out.println("b= ");
-   int c=y%20;
-   System.out.println("c= ");
-
-
-
-    }
-
     
-    }
+    public static void main(String[] args) {
+        //by magnus
+        //gonna give you the day easter is based on the year you give.
+   Scanner scanner = new Scanner(System.in);
+        int y=scanner.nextInt();
+   int a=y%19;
+   System.out.println("a= "+a);
+   int b=y/100;
+   System.out.println("b= "+b);
+   int c=y%100;
+   System.out.println("c= "+c);
+   int d=b/4;
+   System.out.println("d= "+d);
+   int e=b%4;
+   System.out.println("e= "+e);
+   int f=(b+8)/25;
+   System.out.println("f= "+f);
+   int g=(b-f+1)/3;
+   System.out.println("g= "+g);
+   int h=(19 * a + b - d - g + 15)%30;
+   System.out.println("h= "+h);
+   int i=c/4;
+   System.out.println("i= "+i);
+   int k=c%4;
+   System.out.println("k= "+k);
+   int r= (32 + 2 * e + 2 * i - h - k)%7;
+   System.out.println("r= "+r);
+   int m=(a + 11 * h + 22 * r)/451;
+   System.out.println("m= "+m);   
+   int n=(h + r - 7 * m + 114)/31;
+   System.out.println("n= "+n);    
+   int p=(h + r - 7 * m + 114)%31;
+   System.out.println("p= "+p); 
+   System.out.println("Easter in "+y + " falls on "+n+"/"+(p+1));     
+}
+}
+    
+    
 
